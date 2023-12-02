602c9852f2fe1e4ad8fb60a5b7bc7ef7dc6c8ecf
Resn4R <purview-joust.0x@icloud.com>
2023-12-02 14:34:16 +0000

    first commit

---
+++ adventOfCodeD2/DiceMaster.swift
@@ -0,0 +1,14 @@
+//
+//  DiceMaster.swift
+//  adventOfCodeD2
+//
+//  Created by Vito Borghi on 02/12/2023.
+//
+
+import Foundation
+
+struct DiceMaster {
+    func getDiceColours(ofGame game: String) {
+        
+    }
+}
--- adventOfCodeD2Tests/adventOfCodeD2Tests.swift
+++ adventOfCodeD2Tests/adventOfCodeD2Tests.swift
@@ -9,28 +9,10 @@ import XCTest
 @testable import adventOfCodeD2
 
 final class adventOfCodeD2Tests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func testGivenAGameShouldReturnNumberOfEachDice() {
+        let dm = DiceMaster()
+        let game = "Game 1: 4 red, 5 blue, 4 green; 7 red, 8 blue, 2 green; 9 blue, 6 red; 1 green, 3 red, 7 blue; 3 green, 7 red"
+        
+        
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
