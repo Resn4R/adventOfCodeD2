d0d675ae8a01577ffebd33b813fa2539e8eb13c4
Resn4R <purview-joust.0x@icloud.com>
2023-12-02 14:11:21 +0000

    Initial Commit

---
+++ adventOfCodeD2/adventOfCodeD2App.swift
@@ -0,0 +1,17 @@
+//
+//  adventOfCodeD2App.swift
+//  adventOfCodeD2
+//
+//  Created by Vito Borghi on 02/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct adventOfCodeD2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
